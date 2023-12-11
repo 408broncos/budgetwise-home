bca13ce18d33860742d624fc7ef523d0379da3ae
Jordan Cardenas <jordanbc408@gmail.com>
2023-12-11 13:09:38 -0800

    add vue components, graph section and removed unnecessary comments

---
+++ components/BarChart.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <chartjs-bar :chart-data="chartData" :options="chartOptions"></chartjs-bar>
+    </div>
+  </template>
+  
+  <script>
+  import { Bar } from 'vue-chartjs'
+  
+  export default {
+    extends: Bar,
+    props: ['chartData', 'options'],
+    mounted() {
+      this.renderChart(this.chartData, this.options)
+    }
+  }
+  </script>
